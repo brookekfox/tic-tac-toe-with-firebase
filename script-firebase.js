367ecf4e742ed01d845edf4d20d478c4008627e3
brookekfox <brookekfox@gmail.com>
2015-02-01 21:34:10 -0800

    add comments to js file

--- script-firebase.js
+++ script-firebase.js
@@ -2,25 +2,30 @@ var app = angular.module('tictacApp', ['firebase']);
 
 app.controller('tictacController', function ($scope, $firebase) {
 
+	//three 'drawers' to store information about the game
 	$scope.board = $firebase(new Firebase("https://my-tic-tac-toe-1.firebaseio.com/board")).$asArray();
 	$scope.counter = $firebase(new Firebase("https://my-tic-tac-toe-1.firebaseio.com/counter")).$asArray();
 	$scope.players = $firebase(new Firebase("https://my-tic-tac-toe-1.firebaseio.com/players")).$asArray();
 
+	//when the board loads, this function runs
 	$scope.board.$loaded().then(function () {
+		//if the board is empty, populate it with 9 empty entries
 		if ($scope.board.length === 0) {
 			for (var i = 0; i < 9; i++) {
 				$scope.board.$add({letterOnBoard: ''});
 			}
-		} else {
+		} else { //if the board already exists, replace all the entries with empty ones
 			for (var i = 0; i < 9; i++) {
 				$scope.board[i].letterOnBoard = '';
 				$scope.board.$save($scope.board[i]);
 			}
 		}
 
+		//remove the previous game's players from the player drawer
 		for (var i = 0; i < $scope.players.length; i++) {
 			$scope.players.$remove($scope.players[i]);
 		}
+		//if there aren't any current players, add them
 		$scope.players.$add( {
 			xORo: 'x',
 			rowCount: [0,0,0],
@@ -38,27 +43,32 @@ app.controller('tictacController', function ($scope, $firebase) {
         
 	});
 
+	//when the counter is loaded, this function runs
 	$scope.counter.$loaded().then(function () {
+		//if there is nothing stored in the counter 'drawer', add an object containing the turn counter and the current player properties
 		if ($scope.counter.length === 0) {
 			$scope.counter.$add({turnCounter: 0, currentPlayer: 'x'});
-		} else {
+		} else { //if the counter already exists, set the turn counter back to 0 and the current player to x
 			$scope.counter[0].turnCounter = 0;
 			$scope.counter[0].currentPlayer = 'x';
 			$scope.counter.$save($scope.counter[0]);
 		}
 	});
 
+	//when a square is clicked, this function runs
 	$scope.squareClick = function (index) {
-		if (	$scope.counter[0].turnCounter % 2 == 0 && //if it's an even turn (for x)
-				$scope.board[index].letterOnBoard == '' && //if the space is empty
+		//on even turns, on empty squares, on the right turn, update everything to the first player's attributes
+		if (	$scope.counter[0].turnCounter % 2 == 0 &&
+				$scope.board[index].letterOnBoard == '' &&
 				$scope.counter[0].currentPlayer == 'x'
 		  ) {
-			$scope.board[index].letterOnBoard = 'x';
+			$scope.board[index].letterOnBoard = 'x'; //place an x on the board in the square
 			$scope.board.$save($scope.board[index]);
-			$scope.counter[0].turnCounter++;
+			$scope.counter[0].turnCounter++; //increment the turn counter
 			$scope.counter[0].currentPlayer = 'o';
 			$scope.counter.$save($scope.counter[0]);
 
+			//depending on the square clicked, the first player's row/column/diagonal counts need to increase
 			if (index == 0) {
 				$scope.players[$scope.players.length-2].rowCount[0]++;
 				$scope.players[$scope.players.length-2].columnCount[0]++;
@@ -103,10 +113,12 @@ app.controller('tictacController', function ($scope, $firebase) {
 				$scope.players.$save($scope.players[$scope.players.length-2]);
 			}
 
+			//after the 4th turn, we start checking for a winner
 			if ($scope.counter[0].turnCounter >= 4) {
 				$scope.winner();
 			}
 
+		//similarly, for player two
 		} else if (	$scope.counter[0].turnCounter % 2 != 0 &&
 					$scope.board[index].letterOnBoard == '' &&
 					$scope.counter[0].currentPlayer == 'o') {
@@ -163,6 +175,7 @@ app.controller('tictacController', function ($scope, $firebase) {
 			if ($scope.counter[0].turnCounter >= 4) {
 				$scope.winner();
 			}
+		//this is so you can't change the square's game piece
 		} else if ($scope.board[index].letterOnBoard != '') {
 			alert('that space is already taken, dum dum!');
 		}
@@ -170,11 +183,12 @@ app.controller('tictacController', function ($scope, $firebase) {
 
 	$scope.winner = function () {
 		for (var k = 0; k < 3; k++) {
+			//if any of the first player's row/column/diagonal counts is 3, he wins
 			if (	$scope.players[$scope.players.length-2].rowCount[k] == 3 ||
 					$scope.players[$scope.players.length-2].columnCount[k] == 3 ||
 					$scope.players[$scope.players.length-2].diagonalCount[k] == 3) {
 				alert("The Nintendo controller wins!");
-				$scope.players[$scope.players.length-2].winCount++;
+				$scope.players[$scope.players.length-2].winCount++; //increment the player's win count
 				$scope.players.$save($scope.players[$scope.players.length-2]);
 				$scope.reset();
 
@@ -193,6 +207,7 @@ app.controller('tictacController', function ($scope, $firebase) {
 		}
 	}
 
+	//reset the board back to empty without deleting the win counts
 	$scope.reset = function () {
 		if ($scope.board.length === 0) {
 			for (var i = 0; i < 9; i++) {
@@ -219,6 +234,7 @@ app.controller('tictacController', function ($scope, $firebase) {
 		$scope.counter.$save($scope.counter[0]);
 	}
 
+	//reset() with reset of win counts and addition of new players to the players drawer
 	$scope.newGame = function () {
 		if ($scope.board.length === 0) {
 			for (var i = 0; i < 9; i++) {
